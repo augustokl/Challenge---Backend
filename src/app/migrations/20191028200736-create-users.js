eb16c59d43d99c5551009c37d4e45c3f9218e068
Augusto <augusto.longo@sc.senai.br>
2019-10-28 18:09:31 -0300

    migration users

--- src/app/migrations/20191028200736-create-users.js
+++ src/app/migrations/20191028200736-create-users.js
@@ -1,5 +1,35 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {},
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTERGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
 
   down: (queryInterface, Sequelize) => {},
 };
