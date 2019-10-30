0a4b39f265ed65652ebb71421ef6da5a3bb4a7bf
Augusto <augusto.longo@sc.senai.br>
2019-10-30 09:08:57 -0300

    new migrations student

---
+++ src/database/migrations/20191030114135-create-students.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false,
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      peso: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      altura: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('students');
+  },
+};
